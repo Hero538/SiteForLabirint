5d66a66921e95896a9febe172836bf0c12d51850
tempaccforlab <git@github.com>
2019-10-31 16:12:52 +0400

    nothing but something

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.search, name='index'),
+    path('', views.search, name='index'), #ничего не делает потому что ничто ничего не делает
 ]
--- search/views.py
+++ search/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def search(request):
-    return(redirect, 'index')
+    return(redirect, 'index') #ничего не делает 
