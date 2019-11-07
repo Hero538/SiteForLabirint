fd3faa0533026f255f2433f3271d913d7ff8a052
tempaccforlab <git@github.com>
2019-11-07 21:46:46 +0400

    ой точно...

--- materials/urls.py
+++ materials/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('materials/',views.materials,name='materials'),
+    path('materials/physics/mechanics/', views.mechanics, name='mechanics'),
+    path('materials/physics/kinematics/', views.kinematics, name='kinematics')
 
 ]
